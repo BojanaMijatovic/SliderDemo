d6e33c6bd48dd192ab20d7348fe25478b1851ad3
Bojana Mijatovic <bojanamijatovic93@gmail.com>
2019-09-18 09:47:32 +0200

    portfolio page finished

--- webroot/wp-content/themes/onetel-theme/footer.php
+++ webroot/wp-content/themes/onetel-theme/footer.php
@@ -5,14 +5,14 @@
     */
 ?>
 
-<footer>
+<footer class="footer">
   <p> Copyright  	&copy;  2084 Company Name </p>
   <?php wp_nav_menu(
     array(
       'menu' => 'footer-menu',
       'theme_location' => 'footer-menu',
-      'container_class' => 'footer-social',
-      'menu_class' => 'footer-icon',
+      'container_class' => 'footer__social',
+      'menu_class' => 'footer__list',
       )
     );?>
 </footer>
--- webroot/wp-content/themes/onetel-theme/src/js/all.js
+++ webroot/wp-content/themes/onetel-theme/src/js/all.js
@@ -1,10 +1,8 @@
 (function($) {
-  if ($(window).width() < 768) {
     $('.burger, .nav a').on('click', function(e) {
     $('.nav').slideToggle('open');
     $('.container').toggleClass('push');
   });
-}
 })( jQuery );
 
 
